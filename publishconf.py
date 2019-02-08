7bfda8d3b8be40587ea9f71f880b2f845795ab36
Student <ldmcdona@ualberta.ca>
2019-02-07 20:06:05 -0500

    styles

--- publishconf.py
+++ publishconf.py
@@ -15,7 +15,7 @@ SITEURL = 'https://ldmcdona.github.io/cmput404lab5/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+#CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
